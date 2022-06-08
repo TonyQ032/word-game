81ee9529f758f482edd20e10b493b3aa5597ebc2
cesar-infante <cesar261110@gmail.com>
2022-06-07 21:33:31 -0700

    function to button text changes to 'game on!'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-// placeholder
+const element = document.getElementById("start-button")
+element.addEventListener("click", startGame);
+
+function startGame() {
+    document.getElementById("start-game").textContent = "Game On!"
+}
